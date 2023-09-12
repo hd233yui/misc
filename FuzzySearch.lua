113d102530ec1f51ba59f4d07819f919b97c4a04
hd233yui <71934895+hd233yui@users.noreply.github.com>
2023-09-12 18:10:01 +0800

    Update FuzzySearch.lua

--- FuzzySearch.lua
+++ FuzzySearch.lua
@@ -1,9 +1,50 @@
-function getSearchIDs()
-	
+local FuzzySearch = class("FuzzySearch")
+local conf = itemConf -- 用户配置
+
+function FuzzySearch:getSearchIDsByInputValue(searchValue,ids)
+	local searchIds = {}
+	local helpSort = {}
+	local canNext = true
+	local lastIndex = 1
+	local searchCharTable = {}
+	if not self:isEasternLang() then
+		searchCharTabel = string.split(searchValue,"")
+	else
+		while canNext do
+			local nextIndex
+			nextIndex,canNext = utf8.next_raw(searchValue,lastIndex)
+			if canNext then
+				local utf8Char = string.sub(searchValue,lastIndex,nextIndex - 1)
+				table.insert(searchCharTable,utf8Char)
+				lastIndex = nextIndex
+			else
+				local utf8Char = string.sub(searCharTable,lastIndex)
+				table.insert(searchCharTable,utf8Char)
+			end
+		end
+	end
+
+	for i, id in ipairs(ids) do
+		local heroName
+		itemName = string.tolower(conf:getName(id))
+		local matchNum = self:fuzzySearchHelper(searchCharTable,itemName)
+		if matchNum > 0 then
+			table.insert(searchIDs,id)
+			helpSort[tostring(id)] = matchNum
+		end
+	end
+
+	table.sort(searchIDs,function(itemA,itemB)
+		local matchNumA = helpSort[tostring[itemA]]
+		local matchNumB = helpSort[tostring[itemB]]
+		return matchNumA > matchNumB
+	end)
+
+	return searchIDs
 end
 
 -- 用户输入的字符表/匹配的已知目标名
-function fuzzySearchHelper(searchCharTable,itemName)
+function FuzzySearch:fuzzySearchHelper(searchCharTable,itemName)
 	local matchNum = 0
  	for _,char in ipairs(searchCharTable) do
 		if string.trim(char) == "" then
@@ -11,7 +52,7 @@ function fuzzySearchHelper(searchCharTable,itemName)
   		else
 			local newChar = string.gsub(char,"%p",function(c)
    				return "%" .. c
-	  		)
+			end)
 
 			if string.match(itemName,newChar) then
 				matchNum = matchNum + 1
@@ -19,5 +60,7 @@ function fuzzySearchHelper(searchCharTable,itemName)
  		end
   	end
 
-   return matchNum
+	return matchNum
 end
+
+return FuzzySearch
